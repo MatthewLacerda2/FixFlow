9213e381c80b138e3677038196579cc0e8ba3299
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-15 19:08:18 -0300

    notfound navigate

--- fixflow.client/src/Pages/NotFoundPage.tsx
+++ fixflow.client/src/Pages/NotFoundPage.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 
-const NotFoundPage = () => {
+const AppointmentSchedule = () => {
 
   const {id} = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Check if the id matches any of the specified patterns
+    if (id && ['cliente', 'clients', 'clientes'].includes(id.toLowerCase())) {
+      navigate("/Client") ;
+    }
+  }, [id]);
 
   return (
     <div>
-      <h1>Not Found {id}</h1>
+      <h1>Not {id}</h1>
+      <p>You can customize this component as needed.</p>
     </div>
   );
 };
 
-export default NotFoundPage;
+export default AppointmentSchedule;
--- fixflow.client/src/Pages/Secretary/SecretaryPage.tsx
+++ fixflow.client/src/Pages/Secretary/SecretaryPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SecretaryPage = () => {
   return (
     <div>
       <h1>Hello, this is an example component!</h1>
       <p>You can customize this component as needed.</p>
+      <Link to="/RegisterSecretary"/>
     </div>
   );
 };
