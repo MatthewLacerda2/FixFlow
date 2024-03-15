858d2943eaf5d68342aebcfe92eb40c36b50c80d
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-15 18:11:22 -0300

    route sketching

--- FixFlow.Server/Startup.cs
+++ FixFlow.Server/Startup.cs
@@ -64,7 +64,7 @@ public class Startup {
         {
             app.UseDeveloperExceptionPage();
             app.UseSwagger();
-            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flow API v0.5"));
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flow API v1"));
         }
 
         app.UseRouting();
--- fixflow.client/src/Pages/Shared/AppointmentSchedule.tsx
+++ fixflow.client/src/Pages/Shared/AppointmentSchedule.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const AppointmentSchedule = () => {
+
+  const {id} = useParams();
+
   return (
     <div>
-      <h1>Hello, this is an example component!</h1>
+      <h1>Hello, this is {id}</h1>
       <p>You can customize this component as needed.</p>
     </div>
   );
