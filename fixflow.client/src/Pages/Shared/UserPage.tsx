032c10e641a89fb725585bdddbe14632e53e3c64
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-18 01:11:06 -0300

    Actions component

--- fixflow.client/src/Pages/Shared/UserPage.tsx
+++ fixflow.client/src/Pages/Shared/UserPage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from '../../Components/Card';
 import LogExpendable from '../../Components/LogExpandable';
+import Actions from '../../Components/Actions';
 
 const UserPage: React.FC = () => {
   
@@ -13,15 +14,18 @@ const UserPage: React.FC = () => {
   };
 
   return (
-    <div style={{ display: 'flex' }}>
-      <Card
-        title="Log of Appointments"
-        items={generateLogs(2)} // Generating 2 LogExpendables as placeholder data
-      />
-      <Card
-        title="Scheduled Appointments"
-        items={generateLogs(3)} // Generating 3 LogExpendables as placeholder data
-      />
+    <div>
+      <Actions/>
+      <div style={{ display: 'flex' }}>
+        <Card
+          title="Log of Appointments"
+          items={generateLogs(2)} // Generating 2 LogExpendables as placeholder data
+        />
+        <Card
+          title="Scheduled Appointments"
+          items={generateLogs(3)} // Generating 3 LogExpendables as placeholder data
+        />
+      </div>
     </div>
   );
 };
