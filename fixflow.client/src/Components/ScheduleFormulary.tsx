e5e39342d6fcda590a38c2c30bded01fda54c3f9
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-18 16:33:49 -0300

    schedule formulary using schedule data type

--- fixflow.client/src/Components/ScheduleFormulary.tsx
+++ fixflow.client/src/Components/ScheduleFormulary.tsx
@@ -1,57 +1,59 @@
 import { useState } from 'react';
+import ScheduleAppointment from '../Data/ScheduleAppointment';
 
 function ScheduleFormulary() {
-  
-  const [clientId, setClientId] = useState('');
-  const [dateTime, setDateTime] = useState('');
-  const [attendantId, setAttendantId] = useState('');
-  const [secretaryId, setSecretaryId] = useState('');
-  const [expectedPrice, setExpectedPrice] = useState('');
-  const [observation, setObservation] = useState('');
+
+  const [schedule, setSchedule] = useState<ScheduleAppointment>(new ScheduleAppointment());
   const [buttonColor, setButtonColor] = useState('yellow');
 
   const handleSend = () => {
-    if (dateTime && clientId) {
+    if (schedule.dateTime && schedule.clientId) {
       setButtonColor('green');
-      // Logic to send data
+      // Lógica para enviar dados
     }
-  };  
+  };
+
+  const handleChange = (field: string, value: string | Date) => {
+    setSchedule(prevSchedule => ({
+      ...prevSchedule,
+      [field]: value
+    }));
+  };
+
+  const handleNumberChange = (field: string, value: number) => {
+    setSchedule(prevSchedule => ({
+      ...prevSchedule,
+      [field]: value
+    }));
+  };
 
   return (
-    <div>
-      <header className="header">
-        <div className="header-left">
-          FixFlow
-        </div>
-        <div className="header-right">
-          <button>Login</button>
-        </div>
-      </header>
-      <div className="form-container">
-        <div>
-            <label>Client ID:</label>
-            <input type="text" value={clientId} onChange={(e) => setClientId(e.target.value)} required />
-        </div>
-        <div>
-            <label>Date Time:</label>
-            <input type="datetime-local" value={dateTime} onChange={(e) => setDateTime(e.target.value)} required />
-        </div>
-        <div>
-            <label>Attendant ID:</label>
-            <input type="text" value={attendantId} onChange={(e) => setAttendantId(e.target.value)} />
-        </div>
-            <label>Secretary ID:</label>
-            <input type="text" value={secretaryId} onChange={(e) => setSecretaryId(e.target.value)} />
-        <div>
-            <label>Expected Price:</label>
-            <input type="number" value={expectedPrice} onChange={(e) => setExpectedPrice(e.target.value)} />
-        </div>
-        <div>
-            <label>Observation:</label>
-            <textarea value={observation} onChange={(e) => setObservation(e.target.value)} />
-        </div>
-        <button style={{ backgroundColor: buttonColor }} onClick={handleSend}>Send</button>
+    <div className="form-container">
+      <div>
+        <label>Client ID:</label>
+        <input type="text" value={schedule.clientId} onChange={(e) => handleChange('clientId', e.target.value)} required />
       </div>
+      <div>
+        <label>Date Time:</label>
+        <input type="datetime-local" value={schedule.dateTime.toISOString().substring(0, 16)} onChange={(e) => handleChange('dateTime', new Date(e.target.value))} required />
+      </div>
+      <div>
+        <label>Attendant ID:</label>
+        <input type="text" value={schedule.attendantId} onChange={(e) => handleChange('attendantId', e.target.value)} />
+      </div>
+      <div>
+        <label>Secretary ID:</label>
+        <input type="text" value={schedule.secretaryId} onChange={(e) => handleChange('secretaryId', e.target.value)} />
+      </div>
+      <div>
+        <label>Expected Price:</label>
+        <input type="number" value={schedule.expectedPrice} onChange={(e) => handleNumberChange('expectedPrice', parseFloat(e.target.value))} />
+      </div>
+      <div>
+        <label>Observation:</label>
+        <textarea value={schedule.observation} onChange={(e) => handleChange('observation', e.target.value)} />
+      </div>
+      <button style={{ backgroundColor: buttonColor }} onClick={handleSend}>Send</button>
     </div>
   );
 }
