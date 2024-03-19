d68aaf76c791b4be10f58e2fdb0c88a7556442c0
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-19 14:11:06 -0300

    All routes

--- fixflow.client/src/App.tsx
+++ fixflow.client/src/App.tsx
@@ -4,12 +4,13 @@ import Header from './Components/Header/Header';
 import MainPage from './Pages/MainPage';
 import NotFoundPage from './Pages/NotFoundPage';
 import UserPage from './Pages/Shared/UserPage';
-import AppointmentLogPage from './Pages/Shared/AppointmentLogPage';
+import CreateLogPage from './Pages/Shared/CreateLogPage';
 import CreateSchedulePage from './Pages/Shared/CreateSchedulePage';
 import AppointmentSchedulePage from './Pages/Shared/AppointmentSchedulePage';
 import UserSettingsPage from './Pages/Shared/UserSettingsPage';
 import RegisterUser from './Pages/Business/RegisterUser';
 import WeekOverviewPage from './Pages/Business/WeekOverviewPage';
+import AppointmentLogPage from './Pages/Shared/AppointmentLogPage';
 
 function App() {
     return (
@@ -25,6 +26,7 @@ function App() {
                     <Route index element={<UserPage/>}/>
                     <Route path="UserSettings" element={<UserSettingsPage/>}/>
                     <Route path="RegisterClient" element={<RegisterUser/>}/>
+                    <Route path="WeekOverview" element={<WeekOverviewPage/>}/>
                 </Route>
                 <Route path="/Secretary">
                     <Route index element={<UserPage/>}/>
@@ -34,7 +36,8 @@ function App() {
                     <Route path="RegisterSecretary" element={<RegisterUser/>}/>
                     <Route path="WeekOverview" element={<WeekOverviewPage/>}/>
                 </Route>
-                <Route path="/AppointmentLog/:id" element={<AppointmentLogPage/>}/>
+                <Route path="/AppointmentLog/" element={<AppointmentLogPage/>}/>
+                <Route path="/AppointmentLog/:id" element={<CreateLogPage/>}/>
                 <Route path="/AppointmentSchedule" element={<CreateSchedulePage/>}/>
                 <Route path="/AppointmentSchedule/:id" element={<AppointmentSchedulePage/>}/>                
                 <Route path="*" element={<NotFoundPage/>}/>
