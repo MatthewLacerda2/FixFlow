ebcfb7841c91014c198fba588b5b1b163eccb1d8
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-19 00:33:29 -0300

    registeruser

--- fixflow.client/src/App.tsx
+++ fixflow.client/src/App.tsx
@@ -4,12 +4,11 @@ import Header from './Components/Header/Header';
 import MainPage from './Pages/MainPage';
 import NotFoundPage from './Pages/NotFoundPage';
 import UserPage from './Pages/Shared/UserPage';
-import RegisterEmployeePage from './Pages/Secretary/RegisterEmployee';
-import RegisterSecretaryPage from './Pages/Secretary/RegisterSecretary';
 import AppointmentLogPage from './Pages/Shared/AppointmentLogPage';
 import CreateSchedulePage from './Pages/Shared/CreateSchedulePage';
 import AppointmentSchedulePage from './Pages/Shared/AppointmentSchedulePage';
 import UserSettingsPage from './Pages/Shared/UserSettingsPage';
+import RegisterUser from './Pages/Shared/RegisterUser';
 
 //falta register client
 //falta log appointment
@@ -27,12 +26,14 @@ function App() {
                 <Route path="/Employee">
                     <Route index element={<UserPage/>}/>
                     <Route path="UserSettings" element={<UserSettingsPage/>}/>
+                    <Route path="RegisterClient" element={<RegisterUser/>}/>
                 </Route>
                 <Route path="/Secretary">
                     <Route index element={<UserPage/>}/>
                     <Route path="UserSettings" element={<UserSettingsPage/>}/>
-                    <Route path="RegisterEmployee" element={<RegisterEmployeePage/>}/>
-                    <Route path="RegisterSecretary" element={<RegisterSecretaryPage/>}/>
+                    <Route path="RegisterClient" element={<RegisterUser/>}/>
+                    <Route path="RegisterEmployee" element={<RegisterUser/>}/>
+                    <Route path="RegisterSecretary" element={<RegisterUser/>}/>
                 </Route>
                 <Route path="/AppointmentLog/:id" element={<AppointmentLogPage/>}/>
                 <Route path="/AppointmentSchedule" element={<CreateSchedulePage/>}/>
