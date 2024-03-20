40e1a45db36007bc795fcaf9cdbba283bc4758ef
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-20 02:31:17 -0300

    good card, lets goooo!

--- fixflow.client/src/Components/Card/Card.tsx
+++ fixflow.client/src/Components/Card/Card.tsx
@@ -3,21 +3,23 @@ import './Card.css';
 
 interface CardProps {
   title: string;
-  items: React.ReactNode[];
+  items?: React.ReactNode[] | string[]; // Making 'items' optional
+  children?: React.ReactNode; // Adding children prop
 }
 
-const Card: React.FC<CardProps> = ({ title, items }) => {
+const Card: React.FC<CardProps> = ({ title, items, children }) => {
   return (
     <div className="card">
       <h2 className="title">{title}</h2>
       <div className="line"></div>
       <div className="content">
-        {items.map((item) => (
+        {items?.map((item) => (
           <div className="item">
             {item}
           </div>
         ))}
       </div>
+      {children} {/* Render children */}
     </div>
   );
 };
