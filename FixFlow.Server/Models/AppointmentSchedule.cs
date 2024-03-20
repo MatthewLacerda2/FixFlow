5206c3dabfef7ebca4952e2e8fe19b1a4606ea86
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-20 17:11:36 -0300

    POST DO SCHEDULE FUNCIONADOOOOOOO!!!!!!

--- FixFlow.Server/Models/AppointmentSchedule.cs
+++ FixFlow.Server/Models/AppointmentSchedule.cs
@@ -1,19 +1,17 @@
 namespace Server.Models;
-
 public class AppointmentSchedule {
     //Remember, anyone can schedule an appointment
     public Guid Id { get; set; }
-    public string clientId { get; set; } = string.Empty;
-    
+    public string ClientId { get; set; } = string.Empty;
     public string AttendantId { get; set; } = string.Empty;
     public string SecretaryId { get; set; } = string.Empty;
 
     //These are just expected, not mandatory to be kept
-    public float expectedPrice { get; set; } = 30f;
-    public DateTime dateTime { get; set; }
-    public string observation { get; set;} = string.Empty;
+    public float ExpectedPrice { get; set; } = 30f;
+    public DateTime DateTime { get; set; }
+    public string Observation { get; set; } = string.Empty;
 
-    public AppointmentSchedule(string _clientId){
-        clientId = _clientId;
+    public AppointmentSchedule(string clientId){
+        ClientId = clientId;
     }
 }
