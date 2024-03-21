ff144770098b8ba1e33ad4d861f526a256a7e874
Matheus Lacerda <matheus.l1996@gmail.com>
2024-03-21 13:26:37 -0300

    sidebar better

--- fixflow.client/src/Components/SideBar/SideBar.tsx
+++ fixflow.client/src/Components/SideBar/SideBar.tsx
@@ -4,13 +4,13 @@ const SideBar = () => {
   return (
     <div className="sidebar">
       <button className="bar-item">Something</button>
-      <div className='line'></div>
+      <div className='line'/>
       <br/>
       <button className="bar-item">Else</button>
-      <div className='line'></div>
+      <div className='line'/>
       <br/>
       <button className="bar-item">Anything</button>
-      <div className='line'></div>
+      <div className='line'/>
     </div>
   );
 };
