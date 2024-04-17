c994713f9a88e08c5b49848b388aec28baf187d1
Matheus Lacerda <matheus.l1996@gmail.com>
2024-04-17 12:54:34 -0300

    initial setups

--- fixflow.client/src/App.tsx
+++ fixflow.client/src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./Components/Header/Header";
+import MainPage from "./MainPage";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage></MainPage>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ fixflow.client/src/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    <div>
+      <h1 style={{ fontSize: "80px", marginBottom: "20px" }}>NOT FOUND</h1>
+      <p>This is not the content you've been looking for</p>
+    </div>
+  );
+};
+
+export default NotFoundPage;
