4cf66236d396d139746ddb9a15fe78e854fec985
Matthew Lacerda <math2.0@hotmail.com>
2024-04-30 00:54:43 -0300

    NewModels - New Api (#33)
    * swagger.json
    
    * flowapi
    
    ---------
    
    Co-authored-by: Matheus Lacerda <matheus.l1996@gmail.com>

--- fixflow.client/src/FlowApi/models/AptSchedule.ts
+++ fixflow.client/src/FlowApi/models/AptSchedule.ts
@@ -2,18 +2,28 @@
 /* istanbul ignore file */
 /* tslint:disable */
 /* eslint-disable */
+import type { AptContact } from './AptContact';
+import type { Business } from './Business';
+import type { Client } from './Client';
 export type AptSchedule = {
     id: string;
     /**
-     * The Id of the Client who took the Appointment
+     * The Id of the Client who made the Schedule
      */
     clientId: string;
+    client?: Client;
     /**
-     * The Id of the Reminder that precedes this Schedule, if applicable
+     * The Id of the Business who owns this Contact
      */
-    reminderId?: string | null;
+    businessId: string;
+    business?: Business;
     /**
-     * The Date to Contact the Client
+     * The Id of the Contact that precedes this Schedule, if any
+     */
+    contactId: string;
+    contact?: AptContact;
+    /**
+     * The scheduled DateTime of the Appointment
      */
     dateTime?: string;
     price?: number;
