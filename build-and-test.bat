38470f329d6e046b6f5e060ce10b93e6ca7f7dfc
Matheus Lacerda <matheus.l1996@gmail.com>
2024-05-06 18:32:19 -0300

    new build.bat

--- build-and-test.bat
+++ build-and-test.bat
@@ -1,12 +1,15 @@
 cd FixFlow.Server
 
+dotnet restore
+dotnet tool install --global dotnet-ef
+dotnet tool install --global dotnet-aspnet-codegenerator
 dotnet build -c Release .\FixFlow.Server.csproj
 swagger tofile --output .\swagger.json .\bin\Release\net8.0\FixFlow.Server.dll v1
 
-echo "React build"
-
 cd ..\fixflow.client\src
 
+npm install -g npm
+npm install -g react
 openapi -i ..\..\FixFlow.Server\swagger.json -o FlowApi -c axios
 npm run build
 npm run test
