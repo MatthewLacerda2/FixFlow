38a88788d965bcd6309b0d6dbb7ce55b54487639
Matheus Lacerda <matheus.l1996@gmail.com>
2024-05-17 02:45:51 -0300

    email contacts periodic

--- FixFlow.Server/Services/EmailContactsPeriodic.cs
+++ FixFlow.Server/Services/EmailContactsPeriodic.cs
@@ -1,3 +1,7 @@
+using System.Net;
+using System.Net.Mail;
+using Server.Data;
+
 namespace Server.Services;
 
 /// <summary>
@@ -5,17 +9,43 @@ namespace Server.Services;
 /// </summary>
 public class EmailContactsPeriodic : BackgroundService
 {
+    private readonly SmtpClient _smtpClient;
+    private readonly ServerContext _context;
+
+    const string EmailAddress = "my_email_address@hotmail.com";
+    const string EmailPassword = "mystrongpassword1234";
+
+    public EmailContactsPeriodic(ServerContext context)
+    {
+        _context = context;
+
+        _smtpClient = new SmtpClient("my_smtp_server")
+        {
+            Port = 587,
+            EnableSsl = true,
+            Credentials = new NetworkCredential(EmailAddress, EmailPassword),
+        };
+    }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            // Your email sending logic goes here
-            Console.WriteLine("Sending unique email...");
-
-            // Simulate sending an email by waiting for a short duration
-            await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+            await SendEmailToContactsAsync();
+            await Task.Delay(TimeSpan.FromHours(24), stoppingToken);
         }
     }
 
+    private async Task SendEmailToContactsAsync()
+    {
+        var contacts = _context.Contacts
+            .Where(c => c.dateTime.Date == DateTime.Today.Date)
+            .ToArray();
+
+        foreach (var contact in contacts)
+        {
+            var mailMessage = new MailMessage(EmailAddress, contact.Client.Email!, "Reminder", "Your message here");
+            await _smtpClient.SendMailAsync(mailMessage);
+        }
+    }
 }
