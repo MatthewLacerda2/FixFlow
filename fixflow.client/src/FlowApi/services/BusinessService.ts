8d9f10ff549c6b8d1d9685461dc784e9a4871f2b
Matheus Lacerda <matheus.l1996@gmail.com>
2024-05-18 02:33:51 -0300

    FlowApi

--- fixflow.client/src/FlowApi/services/BusinessService.ts
+++ fixflow.client/src/FlowApi/services/BusinessService.ts
@@ -91,7 +91,7 @@ export class BusinessService {
             mediaType: 'application/json',
             errors: {
                 400: `The Client's (PhoneNumber || CPF || Email) does not exist`,
-                500: `Server Error`,
+                500: `Internal Server Error`,
             },
         });
     }
