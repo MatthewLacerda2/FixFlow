a9cc2b136f330df72a316e1e6c404b7ffd0ab8e6
Matheus Lacerda <matheus.l1996@gmail.com>
2024-05-20 14:50:07 -0300

    reset request page

--- fixflow.client/src/Pages/PasswordReset/ResetRequestPage/ResetRequestPage.tsx
+++ fixflow.client/src/Pages/PasswordReset/ResetRequestPage/ResetRequestPage.tsx
@@ -1,32 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../../../Components/Card/Card";
+import FlowButton from "../../../Components/FlowButton/FlowButton";
+import "../../../Styles/Form.css";
+import { AccountsService } from "../../../FlowApi";
 
 const ResetRequestPage: React.FC = () => {
-  const sendEmail = () => {
-    console.log("Email sent!");
+  const [email, setEmail] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+  const [success, setSuccess] = useState<string | null>(null);
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
   };
+
+  const sendEmail = async () => {
+    if (!email.includes("@")) {
+      setError("Digite um email valido");
+    }
+
+    setError(null);
+    setSuccess(null);
+
+    AccountsService.postApiV1AccountsResetEmail(email)
+      .then((response) => {
+        setSuccess(response);
+      })
+      .catch((error) => {
+        setError(error);
+      });
+  };
+
+  if (success) {
+    <Card title="Email enviado">{success}</Card>;
+  }
 
   return (
     <Card title="Reset sua senha">
-      <h2>Digite o Email da sua conta</h2>
+      <h2 style={{ marginTop: "-2px", marginBottom: "5px" }}>
+        Digite o Email da sua conta
+      </h2>
       <p>
-        Você receberá um email com o link por onde poderá resetar sua conta. O
-        link é válido por 15 minutos!
+        Você receberá um email com o link para resetar sua senha. O link é
+        válido por 15 minutos!
       </p>
-      <form onSubmit={sendEmail}>
-        <input type="email" placeholder="Email" />
-        <button type="submit">Send</button>
-      </form>
+
+      <input
+        type="email"
+        placeholder="Email"
+        className="input-field"
+        onChange={handleEmailChange}
+      />
+      <FlowButton text="Send" onClick={sendEmail} />
+
+      {error && <div style={{ color: "#cc0000" }}>{error}</div>}
     </Card>
   );
 };
 
 export default ResetRequestPage;
-
-/*
-sent &&
-    <Card title="Email enviado!">
-        <p>
-            Cheque sua correspondência. Use o link no email para resetar sua senha
-        </p>
-    </Card>*/
