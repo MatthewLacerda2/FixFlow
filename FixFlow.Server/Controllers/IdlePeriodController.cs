043f5b0f156d3b746fd65513537530b1e2b4c2c5
Matheus Lacerda <matheus.l1996@gmail.com>
2024-12-08 23:50:17 -0300

    get idle periods endpoint

--- FixFlow.Server/Controllers/IdlePeriodController.cs
+++ FixFlow.Server/Controllers/IdlePeriodController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Server.Data;
 using Server.Models;
 using Server.Models.Erros;
@@ -30,6 +31,12 @@ public class IdlePeriodController : ControllerBase {
 	[HttpPost]
 	public async Task<IActionResult> CreateIdlePeriod([FromBody] IdlePeriod idlePeriod) {
 
+		string businessId = User.Claims.First(c => c.Type == "businessId")?.Value!;
+
+		if (idlePeriod.BusinessId != businessId) {
+			return BadRequest(ValidatorErrors.BadIdlePeriodOwnership);
+		}
+
 		idlePeriod.Id = Guid.NewGuid().ToString();
 		idlePeriod.Name = StringUtils.PhraseCaseNormalizer(idlePeriod.Name)!;
 
@@ -37,6 +44,23 @@ public class IdlePeriodController : ControllerBase {
 		await _context.SaveChangesAsync();
 
 		return CreatedAtAction(nameof(CreateIdlePeriod), idlePeriod);
+	}
+
+	/// <summary>
+	/// Gets Idle Periods owned by the Company that start and end within a given time-period
+	/// </summary>
+	[ProducesResponseType(StatusCodes.Status201Created, Type = typeof(IdlePeriod))]
+	[HttpGet]
+	public async Task<IActionResult> GetIdlePeriod(DateTime startDate, DateTime finishDate) {
+
+		string businessId = User.Claims.First(c => c.Type == "businessId")?.Value!;
+
+		IdlePeriod[] periods = await _context.IdlePeriods
+		.Where(i => i.Start >= startDate)
+		.Where(i => i.Finish <= finishDate)
+		.ToArrayAsync();
+
+		return Ok(periods);
 	}
 
 	/// <summary>
